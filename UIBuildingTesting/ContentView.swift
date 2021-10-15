6303fa4a35fe972717276f447b9ac683b377b6a4
Ethan Smith <edhsmith@icloud.com>
2021-10-16 08:14:52 +1300

    Initial Commit

---
+++ UIBuildingTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIBuildingTesting
+//
+//  Created by Ethan Smith on 16/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIBuildingTesting/UIBuildingTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBuildingTestingApp.swift
+//  UIBuildingTesting
+//
+//  Created by Ethan Smith on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UIBuildingTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
